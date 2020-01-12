af52573e268af5bab84c1352480f1fd50009c3bc
Blas Vinas <blasvinas@gmail.com>
2020-01-11 22:02:09 -0500

    delete old projects

--- Python for Programers/examples/My Examples/arbitrary_arguments.py
+++
@@ -1,7 +0,0 @@
-# arbitraty_arguments.py
-''' Shows how to use an arbitrary number of arguments '''
-
-def average(*args):
-    return sum(args)/len(args)
-
-print(f'Average of 5,8,2,4,6,9 is  { average(5,8,2,4,6,9)}')
--- Python for Programers/examples/My Examples/keyword_arguments.py
+++
@@ -1,9 +0,0 @@
-# default_parameters.py
-""" Shows how to use functions with keyword arguments """
-
-def rectangle_area(length, width):
-    return length * width
-
-print(f'Area of a rectangle with length: 2 and width: 3 is {rectangle_area(width=3, length=2)}')
-print(f'Area of a rectangle with length: 5 and width: 3 is {rectangle_area(length=5, width=3)}')
-print(f'Area of a rectangle with length: 7 and width: 4 is {rectangle_area(7,4)}')
